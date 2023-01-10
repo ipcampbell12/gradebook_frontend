c69d3a025bd9b20e65db2c0d864803fc4b142b10
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-10 10:24:15 -0800

    signin components

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+// import Signup from "./Signup"
+// import Login from "./Login"
+// import Dashboard from "./Dashboard"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello to your mom!</h1>
+    </div>
+  );
+}
+
+export default App;
