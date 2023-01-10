d42d2979c2a0ad89130eb2b2631362f519a86716
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-10 12:36:46 -0800

    appbar, table, button, state

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,45 @@
 // import Login from "./Login"
 // import Dashboard from "./Dashboard"
 
+import Dashboard from "./Dashboard"
+import React, { useState } from 'react'
+
+
 function App() {
+
+  const [studentListState, setStudentListState] = useState([])
+  const [subjectListState, setSubjectListState] = useState([])
+  const [assessmentListState, setAssessmentListState] = useState([])
+  
+  
+  const addStudent = (student) =>{ 
+    const id = studentListState.length + 1
+    const newStudent = { id, ...student}
+
+    setStudentListState([...studentListState, newStudent])
+
+  }
+
+  const addSubject = (subject) =>{ 
+    const id = subjectListState.length + 1
+    const newSubject = { id, ...subject}
+
+    setSubjectListState([...subjectListState, newSubject])
+
+  }
+
+  const addAssessment = (assessment) =>{ 
+    const id = assessmentListState.length + 1
+    const newAssessment = { id, ...assessment}
+
+    setAssessmentListState([...assessmentListState, newAssessment])
+
+  }
+
   return (
     <div className="App">
-      <h1>Hello to your mom!</h1>
+      <Dashboard onSubject={addSubject} onStudent={addStudent} onAssessment ={addAssessment} />
+      
     </div>
   );
 }
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,1 +1,17 @@
 // import React, { useRef, useState } from "react"
+import React from 'react';
+import Navigation from "./Navigation"
+import StudentList from "./StudentList"
+import AddStudentButton from "./AddStudentButton"
+
+function Dashboard(props) {
+    return (
+        <div>
+            <Navigation />
+            <StudentList />
+            <AddStudentButton />
+        </div>
+    );
+}
+
+export default Dashboard;
