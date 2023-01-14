e1b31612f07b9c066c3594aabf3de144d7ce12df
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-14 09:51:14 -0800

    api call for bulk add

--- src/Components/APIService.js
+++ src/Components/APIService.js
@@ -22,8 +22,9 @@ export default class APIServce {
             .then(resp => resp.json())
     }
 
-    static addStudentAssessment(body) {
-        return fetch('http://127.0.0.1:5001//student/<string:student_id>/assessment/<string:assessment_id>', {
+    //bulk score load
+    static addClassScores(body, teacher_id, assessment_id) {
+        return fetch(`http://127.0.0.1:5001///teacherstudents/${teacher_id}/assessment/${assessment_id}`, {
             'method': 'POST',
             headers: {
                 'Content-Type': 'application/json'
