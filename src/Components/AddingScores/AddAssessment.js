741ef42fe622fd2857be31c49bdc7eafe0b6a766
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-26 21:38:10 -0800

    select subject when adding assessment

--- src/Components/AddingScores/AddAssessment.js
+++ src/Components/AddingScores/AddAssessment.js
@@ -4,6 +4,10 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import APIServce from '../../APIService';
 import Alert from 'react-bootstrap/Alert';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { FormGroup } from '@mui/material';
 
 
 export default function AddAssessment({ subjects, onAssessment, moduleState }) {
@@ -14,14 +18,12 @@ export default function AddAssessment({ subjects, onAssessment, moduleState }) {
     //console.log(name)
     const [scored, setScored] = useState(false)
     const [show, setShow] = useState(false)
+    const [subjectId, setSubjectId] = useState('')
 
     const onSubmit = (e) => {
         e.preventDefault()
 
-
-        const subject = subjects.find(subject => subject.id === 1)
-
-        const subject_id = subject.id
+        const subject_id = subjectId
 
         setScored(false)
         //send data to API
@@ -30,23 +32,53 @@ export default function AddAssessment({ subjects, onAssessment, moduleState }) {
             .catch(error => console.log(error))
 
         //send data to UI
-        onAssessment({ name, subject_id, scored })
+        onAssessment({ name, subjectId, scored })
         setShow(true)
 
         setName('')
 
     }
 
+    const onClick = (e) => {
+        const value = e.target.value
+        setSubjectId(value)
+
+    }
+
+
+
 
     return (
         <div>
             <Form>
-                <Form.Group className="mb-3" onSubmit={onSubmit}>
+                <FormGroup className="mb-3" onSubmit={onSubmit}>
                     <Form.Label>Assessment Name</Form.Label>
                     <Form.Control type="text" placeholder="Assessment Name" value={module.name} onChange={(e) => {
                         setName(e.target.value)
                     }} />
-                </Form.Group>
+                </FormGroup>
+                <FormControl fullWidth>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Age"
+                        onChange={onClick}
+                        value={"Choose an assessment"}
+                    >
+                        {
+                            subjects.map(subject => {
+
+                                return (<MenuItem className="menu" id={`assessment-${subject.id}`} key={subject.id} value={subject.id} onChange={onClick}>
+                                    {subject.name}
+
+                                </MenuItem>)
+
+
+                            })
+
+                        }
+                    </Select>
+                </FormControl>
                 <Button variant="primary" type="submit" onClick={onSubmit}>
                     Create Assessment
                 </Button>
@@ -55,7 +87,7 @@ export default function AddAssessment({ subjects, onAssessment, moduleState }) {
                 </Alert>}
             </Form>
 
-        </div>
+        </div >
     );
 }
 
