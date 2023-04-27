fb79c1d4c044b25afc5a18bd6ede23ce185e1de4
ipcampbell12 <ipcampbell12@gmail.com>
2023-04-26 17:40:45 -0700

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import Dashboard from "./Components/Dashboard/Dashboard"
-import React from 'react'
+//import Dashboard from "./Components/Dashboard/Dashboard"
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from "./Components/Dashboard/Navigation"
-import EditStudent from "./Components/AddingStudents/EditStudent";
-import Grade from "./Components/AddingScores/Grade";
+//import EditStudent from "./Components/AddingStudents/EditStudent";
+//import Grade from "./Components/AddingScores/Grade";
 import LandingPage from "./Components/AddTeachers/LandingPage"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TeacherContextProvider from "./Context/TeacherContext";
 import { Helmet } from "react-helmet";
 
-
-
+const Dashboard = lazy(() => import('./Components/Dashboard/Dashboard'))
+const EditStudent = lazy(() => import('./Components/AddingStudents/EditStudent'))
+const Grade = lazy(() => import('./Components/AddingScores/Grade'))
 
 function App() {
 
@@ -28,9 +29,9 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
 
-            <Route path="/dashboard" exact element={<Dashboard />} />
-            <Route path="/student" element={<EditStudent />} />
-            <Route path="/grade" exact element={<Grade />} />
+            <Route path="/dashboard" exact element={<Suspense fallback={<p>Loading . . .</p>}><Dashboard /></Suspense>} />
+            <Route path="/student" element={<Suspense fallback={<p>Loading . . .</p>}><EditStudent /></Suspense>} />
+            <Route path="/grade" exact element={<Suspense fallback={<p>Loading . . .</p>}><Grade /></Suspense>} />
           </Routes>
 
 
